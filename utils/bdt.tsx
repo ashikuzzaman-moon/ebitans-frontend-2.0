89830dcf0e9f538d9ffb5a9e98d811adb37ba498
jewel1269 <mohammadjewel4k5@gmail.com>
2024-11-20 18:51:08 +0600

    final 11

--- utils/bdt.tsx
+++ utils/bdt.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { RootState } from '@/redux/store';
+import { useSelector } from 'react-redux';
+
 const BDT = ({ price }: any) => {
     const home = useSelector((state: RootState) => state.home); // Access updated Redux state
     const { currency } = home?.header || {};
